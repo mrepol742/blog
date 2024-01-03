760e1c7bf9b928a78c11d97aa560559db248db50
Melvin Jones Repol <mrepol742@gmail.com>
2024-01-03 19:56:08 +0800

    Initial Commit

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -6,6 +6,19 @@ module.exports = {
   theme: '@vuepress/theme-blog',
   description: description,
   head: [
+    ['script', {
+      "data-name": "BMC-Widget",
+      "data-cfasync": false,
+      src: 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js',
+      "data-id": 'mrepol742',
+      "data-description": "Support me on Buy me a coffee!",
+      "data-message": "",
+      "data-color": "#5F7FFF",
+      "data-position": "Right",
+      "data-x_margin": "18",
+      "data-y_margin": "18"
+    }],
+
     // Twitter
     ['meta', { name: 'twitter:site', content: '@mrepol742' }],
     ['meta', { name: 'twitter:domain', content: 'mrepol742.github.io' }],
